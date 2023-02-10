d0685955ce6882297368bfc4eab9e69aebfe5aff
mingun choi <ryanchoi100826@gmail.com>
2023-02-10 00:30:49 -0500

    modified database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,25 +1,43 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+// console.log('Connected to database!')
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
 /// Users
+
 
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+// const getUserWithEmail = function(email) {
+//   let user;
+//   for (const userId in users) {
+//     user = users[userId];
+//     if (user.email.toLowerCase() === email.toLowerCase()) {
+//       break;
+//     } else {
+//       user = null;
+//     }
+//   }
+//   return Promise.resolve(user);
+// }
+// exports.getUserWithEmail = getUserWithEmail;
+
+const getUserWithEmail = (email) => {
+
+  return pool
+  .query (`SELECT * FROM users WHERE email = $1`, [email])
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message)
+    return null;
+  })
+};
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -27,8 +45,21 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+// const getUserWithId = function(id) {
+//   return Promise.resolve(users[id]);
+// }
+// exports.getUserWithId = getUserWithId;
+
+const getUserWithId = (id) => {
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1`, [id])
+  .then((result) => {
+    console.log(result.rows)
+    return result.rows[0];
+  })
+  .catch((err) => {
+    return err.message;
+  })
 }
 exports.getUserWithId = getUserWithId;
 
@@ -38,14 +69,29 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+// const addUser =  function(user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// }
+// exports.addUser = addUser;
+const addUser = (user) => {
+  const {name, email, password} = user;
+  return pool
+  .query(`
+  INSERT INTO users (id, name, email, password) 
+  VALUES(DEFAULT, $1, $2, $3) 
+  RETURNING *;`, [name, email, password])
+  .then((result) => {
+    console.log(result.rows)
+    return result.rows;
+  })
+  .catch((err) => {
+    return err.message;
+  })
 }
 exports.addUser = addUser;
-
 /// Reservations
 
 /**
@@ -66,14 +112,32 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.getAllProperties = getAllProperties;
+
+
+// const getAllProperties = function(options, limit = 10) {
+//   const limitedProperties = {};
+//   for (let i = 1; i <= limit; i++) {
+//     limitedProperties[i] = properties[i];
+//   }
+//   return Promise.resolve(limitedProperties);
+// }
+
+
+
+
+
+
 
 
 /**
